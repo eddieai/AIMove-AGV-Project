5553becf9844b478d0b93830a4a2dbf706076472
eddie <eddie.ai@hotmail.com>
2020-02-06 16:07:32 +0100

    Depth clean function

--- main_numpy.py
+++ main_numpy.py
@@ -4,11 +4,16 @@ import json
 import os
 import re
 import numpy as np
+from numpy import genfromtxt
+import csv
+from tslearn.metrics import dtw
 import matplotlib.pyplot as plt
 import pyrealsense2 as rs
 # import pandas as pd
 from time import time
+from itertools import combinations
 import warnings
+
 warnings.filterwarnings("ignore")
 
 # Configure depth and color streams
@@ -19,12 +24,14 @@ max_image_Y = 480
 config.enable_stream(rs.stream.depth, max_image_X, max_image_Y, rs.format.z16, 15)
 config.enable_stream(rs.stream.color, max_image_X, max_image_Y, rs.format.bgr8, 15)
 
+
 # Start streaming
 pipeline.start(config)
 align_to = rs.stream.color
 align = rs.align(align_to)
 
 from initialize_OP import *
+
 # Starting OpenPose
 params['number_people_max'] = 1
 opWrapper = op.WrapperPython()
@@ -32,13 +39,33 @@ opWrapper.configure(params)
 opWrapper.start()
 
 # Initialize numpy matrix of result
-data_window = np.zeros((0,7,4))
+data_window = np.zeros((0, 7, 4))
 
 # Number of time lenght (defined pas fps)
-max_frame_iter = 100
+max_frame_iter = 90
 
 plt.ion()
 fig = plt.figure()
+
+
+def depth_cleaned(data_window, iter = 1):
+    for _ in range(iter):
+        depth_median = np.median(data_window[:, :, 3], axis=0)
+        depth_median_upper = depth_median + 0.1
+        depth_median_lower = depth_median - 0.1
+
+        depth_frame_before = np.roll(data_window[:, :, 3], 1, axis=0)
+        depth_frame_after = np.roll(data_window[:, :, 3], -1, axis=0)
+
+        depth_to_replace = np.logical_or(data_window[:, :, 3] < depth_median_lower,
+                                         data_window[:, :, 3] > depth_median_upper)
+
+        data_window[:, :, 3] = data_window[:, :, 3] * np.invert(depth_to_replace) + depth_to_replace * (
+                    (depth_frame_before + depth_frame_after) / 2)
+    return data_window
+
+
+
 
 try:
     # Initialize variable iteration of frames
@@ -86,67 +113,97 @@ try:
         datum.cvInputData = color_image
         opWrapper.emplaceAndPop([datum])
         opData = datum.poseKeypoints
-        #print('datum.poseKeypoints.shape :', datum.poseKeypoints.shape, '\n')
+        # print('datum.poseKeypoints.shape :', datum.poseKeypoints.shape, '\n')
 
         X = np.empty((0))
         Y = np.empty((0))
         Depth = np.empty((0))
         Probability = np.empty((0))
 
-        data_joint = np.zeros((0,4))
+        data_joint = np.zeros((0, 4))
         for joint in range(1, 8):
             x = opData[0][joint][0].item()
             y = opData[0][joint][1].item()
             probability = opData[0][joint][2].item()
-            depth = img_depth[min(color_image.shape[0]-1, round(y)), min(color_image.shape[1]-1, round(x))]
+            depth = img_depth[min(color_image.shape[0] - 1, round(y)), min(color_image.shape[1] - 1, round(x))]
 
             X = np.append(X, min(img_depth.shape[1] - 1, round(x)))
             Y = np.append(Y, min(img_depth.shape[0] - 1, round(y)))
             Probability = np.append(Probability, probability)
             Depth = np.append(Depth, depth)
 
-            data_keypoint = np.array([x/max_image_X, y/max_image_Y, probability, depth])
+            data_keypoint = np.array([x / max_image_X, y / max_image_Y, probability, depth])
             data_joint = np.vstack((data_joint, data_keypoint))
 
         data_window = np.vstack((data_window, data_joint.reshape(1, 7, 4)))
 
-        plt.subplot(2, 1, 1)
-        plt.scatter(X, Y, c='white')
-        for i in range(0,7):
-            plt.text(X[i], Y[i], "%.02f" % Probability[i], fontdict=dict(color='white', size='15'), bbox=dict(fill=False, edgecolor='red', linewidth=1))
-
-        plt.subplot(2, 1, 2)
-        plt.scatter(X, Y, c='white')
-        for i in range(0,7):
-            plt.text(X[i], Y[i], "%.02f" % Depth[i], fontdict=dict(color='black', size='15'), bbox=dict(fill=False, edgecolor='red', linewidth=1))
-
-        plt.waitforbuttonpress(0.0001)
-        plt.show()
+        # Plot the current depth (visualization)
+        # plt.subplot(2, 1, 1)
+        # plt.scatter(X, Y, c='white')
+        # for i in range(0,7):
+        #     plt.text(X[i], Y[i], "%.02f" % Probability[i], fontdict=dict(color='white', size='15'), bbox=dict(fill=False, edgecolor='red', linewidth=1))
+        #
+        # plt.subplot(2, 1, 2)
+        # plt.scatter(X, Y, c='white')
+        # for i in range(0,7):
+        #     plt.text(X[i], Y[i], "%.02f" % Depth[i], fontdict=dict(color='black', size='15'), bbox=dict(fill=False, edgecolor='red', linewidth=1))
+        #
+        # plt.waitforbuttonpress(0.0001)
+        # plt.show()
 
         if not args[0].no_display:
-             cv2.imshow("OpenPose 1.5.0 - Tutorial Python API", datum.cvOutputData)
-             key = cv2.waitKey(15)
-             if key == 27: break
+            cv2.imshow("OpenPose 1.5.0 - Tutorial Python API", datum.cvOutputData)
+            key = cv2.waitKey(15)
+            if key == 27: break
 
         frame_iter = frame_iter + 1
-        print('frame_iter :', frame_iter, '\tn_frame :', n_frame)
+        # print('frame_iter :', frame_iter, '\tn_frame :', n_frame)
 
         if frame_iter == max_frame_iter:
             # Create DataFrame of keypoints
-            print('data window shape:',data_window.shape)
+            print('data window shape:', data_window.shape)
             end_time = time()
             run_time = end_time - start_time
             print('run time', run_time)
 
+            data_window = depth_cleaned(data_window, iter=5)
+            data_window_distance = np.empty([max_frame_iter,1])
+
+            for pair in list(combinations(np.arange(0,7), 2)):
+                x1 = data_window[:, pair[0], 0]
+                x2 = data_window[:, pair[1], 0]
+                y1 = data_window[:, pair[0], 1]
+                y2 = data_window[:, pair[1], 1]
+                depth1 = data_window[:, pair[0], 3]
+                depth2 = data_window[:, pair[1], 3]
+                distance = np.sqrt((x1 - x2)**2 + (y1 - y2)**2 + (depth1 - depth2)**2)
+                data_window_distance = np.hstack((data_window_distance, distance.reshape(1,max_frame_iter).T))
+
+            data_window_distance = data_window_distance[:,1:] / np.max(data_window_distance[:,1:])
+           # print(data_window_distance)
+
+            # DTW
+
+            # = genfromtxt('dtw_data.csv', delimiter='"')
+            dtw_train_data = list(csv.reader(open('/home/aimove/Desktop/AIMove AGV Project/code/dtw_data.csv')))
+            dtw_train_data = np.array(dtw_train_data)
+            #print(dtw_train_data)
+            dist = np.empty(7)
+            for i in range(7):
+                dist[i] = dtw(np.array(dtw_train_data[i]), data_window_distance)
+            dtw_pred = np.argmin(dist) + 1
+            print(dtw_pred)
+
+
             # Update variable iteration of frames
             frame_iter = max_frame_iter - 1
             # Delete first keypoints (first frame of the dataframe)
-            data_window = data_window[1:,:,:]
+            data_window = data_window[1:, :, :]
 
-        print(data_window)
 
-        depth_median = np.median(data_window[:, :, 3], axis=0)
+
+
 
 finally:
     # Stop streaming
-     pipeline.stop()
+    pipeline.stop()
